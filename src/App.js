e2571658c2ed5feaffb3864954205e8e64da21ea
Paula.Carvalho <paulacac1@gmail.com>
2020-12-22 00:28:43 -0300

    Atividade Previa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Routes from './Routes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [list, setList] = useState([
+    { name: 'Paula', email: 'paula@algo.com', password: '1234' },
+    { name: 'Teste', email: 'teste@algo.com', password: '1234' },
+  ]);
+
+  function handleInclude(newUser) {
+    const newUsers = [...list, newUser];
+
+    setList(newUsers);
+    console.log(newUsers);
+  }
+
+  return <Routes list={list} include={handleInclude} />;
 }
 
 export default App;
